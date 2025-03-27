a27ea3b207cda8e586e7d357a646981e12b67ff8
R0G3RIO <rogerio.adrian@gmail.com>
2025-03-27 18:38:35 -0300

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,1 +1,15 @@
-// Aqui virá o conteúdo do App com Home e Admin
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Admin from "./Admin";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
+  );
+}
